1084c446588b412f53e4ede524955e7bc3d50e41
jbedoyav <jbedoyav@grupo-exito.com>
2024-03-29 12:08:43 -0500

    🐛 FIX: firebase routes

--- lib/main/main_production.dart
+++ lib/main/main_production.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_clean_architecture/firebase_options_prod.dart';
+import 'package:flutter_clean_architecture/configs/firebase/firebase_options_prod.dart';
 import 'package:flutter_clean_architecture/main.dart';
 import 'package:flutter_clean_architecture/main/app_env.dart';
 
