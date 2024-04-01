4869d91c7c1983dc1e1545f43529ae2352a9ec69
jbedoyav <jbedoyav@grupo-exito.com>
2024-03-31 21:11:36 -0500

    📦 NEW: add some constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,26 @@
+class Constants {
+  // Firebase Collections
+  static const String myDiscountShareDescription = 'myDiscountShareDescription';
+  static const String nativeVideo = 'nativeVideoInApp';
+  static const String couponsLimit = 'couponsLimit';
+  static const String postalCode = 'postalCode';
+  static const String dependencyCode = 'dependencyCode';
+  static const String categoryMyDiscount = 'category_my_discount';
+  static const String firestoreErrorData =
+      'FireStore: Unable to get data from collection.';
+  static const String exitoPayLobby = 'nativeExitoPayLobby';
+  static const String exitoPayHelpCenterLobby = 'nativeExitoPayHelpCenter';
+  static const String exitoPayAlerts = 'nativeExitoPayAlerts';
+  static const String exitoPayAlertsConfirmPayQR = 'confirmPayQRAlert';
+  static const String exitoPayAlertsConfirmSendMoney = 'confirmSendMoney';
+  static const String exitoPayAlertsQrPaymentAnnulled =
+      'confirmQRPaymentAnnulled';
+  static const String exitoPayValues = 'nativeExitoPayValues';
+  static const String exitoPayValuesSendMoneySuggestedAmounts =
+      'sendMoneySuggestedAmounts';
+  static const String nativeHomestore = 'SettingStoreFront';
+  static const String commonBanners = 'bannerslobbyprehome';
+  static const String lobbyFloatingHelpButton = 'lobbyFloatingHelpButton';
+  static const String appConfigCollection = 'appConfig';
+  static const String exitoExpress = 'exitoExpress';
+}
